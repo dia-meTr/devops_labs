b223ee19f9e0e6782d7f1f98ccc7f6632c7bbd33
dia-me-tr <diana.tryhub@gmail.com>
2022-11-01 11:13:12 +0200

    latest

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+})
+
+app.listen(80, () => {
+    console.log("App running on port 80");
+
+})
