cb57626c288cd1483475c264cb0bec5712c6b3b6
Diana <49411624+dia-meTr@users.noreply.github.com>
2022-12-13 00:16:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
+  
   res.send('Hello world')
+
 })
 
 app.listen(80, () => {
+
   console.log('App running on port 80')
 
 })
